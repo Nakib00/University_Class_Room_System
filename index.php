cf127314890039495a2621fe5e2630126bc207f2
nakibul islam <2022142@iub.edu.bd>
2021-11-02 23:36:25 +0600

    kk

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!-- Include navbar -->
 <?php include('navbar.php')?>
 
+<!-- Include javascript file  -->
+<?php include('javascript.php')?>
+
 <!doctype html>
 <html lang="en">
 
@@ -23,50 +26,11 @@
         }
     </style>
 
-    <!-- javascript file add  -->
-
-    <script>
-        // Event for click button
-        function one_clicked() {
-            document.location.href = "01_button.php";
-        }
-
-        function two_clicked() {
-            document.location.href = "02_button.php";
-        }
-
-        function three_clicked() {
-            document.location.href = "03_button.php";
-        }
-
-        function fore_clicked() {
-            document.location.href = "04_button.php";
-        }
-
-        function five_clicked() {
-            document.location.href = "05_button.php";
-        }
-
-        function six_clicked() {
-            document.location.href = "06_button.php";
-        }
-
-        function saven_clicked() {
-            document.location.href = "07_button.php";
-        }
-
-        function eight_clicked() {
-            document.location.href = "08_button.php";
-        }
-    </script>
-
 
     <title>TCMS</title>
 </head>
 
 <body>
-
-    <br><br><br>
     <!-- Using heading title -->
     <div>
         <H1 class="title">Welcome to the TCMS Dashboard</H1>
