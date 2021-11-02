0e12ab0099bd8a2405b71841178fced5bd132f9f
nakibul islam <2022142@iub.edu.bd>
2021-11-02 23:53:35 +0600

    sdf

--- 01_button.php
+++ 01_button.php
@@ -21,47 +21,8 @@
         .title {
             text-align: center;
         }
-
-        /* Table style */
-
-        table {
-            position: absolute;
-            z-index: 2;
-            left: 45%;
-            top: 30%;
-            width: 50%;
-            border-collapse: collapse;
-            border-spacing: 0;
-            box-shadow: 0 2px 15px rgba(64, 64, 64, .7);
-            border-radius: 12px 12px 0 0;
-            overflow: hidden;
-        }
-
-        td,
-        th {
-            padding: 15px 20px;
-            text-align: center;
-        }
-
-        th {
-            background-color: black;
-            color: #fafafa;
-            font-family: 'Open Sans', Sans-serif;
-            font-weight: 200;
-            text-transform: uppercase;
-
-        }
-
-        tr {
-            width: 100%;
-            background-color: #fafafa;
-            font-family: 'Montserrat', sans-serif;
-        }
-
-        tr:nth-child(even) {
-            background-color: #eeeeee;
-        }
     </style>
+    <?php include('Table_css.php') ?>
 
     <title>TCMS</title>
 
