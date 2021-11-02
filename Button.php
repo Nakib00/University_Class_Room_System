490a610085e415607192b120b7ee330030b1c116
nakibul islam <2022142@iub.edu.bd>
2021-11-02 23:45:10 +0600

    fad

---
+++ Button.php
@@ -0,0 +1,57 @@
+    <!-- using button -->
+    
+    <div class="container">
+        <div class="container-a">
+            <div class="row">
+                <div class="col">
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <button class="btn btn-outline-info" type="button" onclick="one_clicked()">Classroom requirement</button><br>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <button class="btn btn-outline-info" type="button" onclick="two_clicked()">Course enrolment distribution</button><br>
+                    </div>
+                </div>
+            </div>
+            <div class="container-b">
+                <div class="row">
+                    <div class="col">
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-outline-info" type="button" onclick="three_clicked()">Usage of resources</button><br>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-outline-info" type="button" onclick="fore_clicked()">IUB available resources</button><br>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-c">
+                    <div class="row">
+                        <div class="col">
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button class="btn btn-outline-info" type="button" onclick="five_clicked()">Availability and course offering</button><br>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button class="btn btn-outline-info" type="button" onclick="six_clicked()">Revenue of IUB</button><br>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container-d">
+                        <div class="row">
+                            <div class="col">
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button class="btn btn-outline-info" type="button" onclick="saven_clicked()">Revenue Engineering School</button><br>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <button class="btn btn-outline-info" type="button" onclick="eight_clicked()">students Enrollment</button><br>
+                                </div>
+                            </div>
+                        </div>
