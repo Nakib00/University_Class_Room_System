f9cdbca668ac571c403c0480bf0c8c0ffb355fbc
nakibul islam <2022142@iub.edu.bd>
2021-11-02 23:59:21 +0600

    jnjk

--- 03_button.php
+++ 03_button.php
@@ -1,5 +1,5 @@
 <!-- Include navbar -->
-<?php include('navbar.php')?>
+<?php include('navbar.php') ?>
 
 <!doctype html>
 <html lang="en">
@@ -9,19 +9,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- CSS File Add  -->
-    <style>
-        .nav-bd {
-            background-color: #CAC9F6;
-        }
-
-        .title {
-            text-align: center;
-        }
-    </style>
+    <!-- Include CSS  -->
+    <?php include('CSS.php') ?>
+    <!-- Include Table CSS -->
+    <?php include('Table_css.php') ?>
 
     <title>TCMS</title>
 </head>
