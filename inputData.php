cf761450131a8cc7381181bf03f180e3b38ba2a3
nakibul islam <2022142@iub.edu.bd>
2021-12-01 18:15:44 +0600

    add data input page

---
+++ inputData.php
@@ -0,0 +1,29 @@
+<!-- Include navbar -->
+<?php include('navbar.php') ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Include CSS  -->
+    <?php include('CSS/CSS.php') ?>
+    <!-- Include Table CSS -->
+    <?php include('CSS/Table_css.php') ?>
+
+    <title>TCMS</title>
+</head>
+
+<body>
+    <!-- Using heading title -->
+    <div>
+        <H1 class="title">Input Data</h1>
+    </div>
+    <hr>
+
+</body>
+
+</html>
--- navbar.php
+++ navbar.php
@@ -1,19 +1,16 @@
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">TCMS</a>
+    <a class="navbar-brand" href="base.php">TCMS</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="base.php">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="register.php">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
+                <a class="nav-link" href="inputData.php">Input Data</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="logout.php">Logout</a>
