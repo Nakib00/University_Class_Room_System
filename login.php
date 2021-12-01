ac48890fb923355fc84ac3fa21ec392873eb21df
nakibul islam <2022142@iub.edu.bd>
2021-12-01 18:32:37 +0600

    sa

--- login.php
+++ login.php
@@ -85,12 +85,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 <li class="nav-item">
                     <a class="nav-link" href="login.php">Login</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
-
-
-
             </ul>
         </div>
     </nav>
