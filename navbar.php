0983c674ec93b561b6a96fc207fc660afc52782e
nakibul islam <2022142@iub.edu.bd>
2021-12-03 23:14:57 +0600

    aa

--- navbar.php
+++ navbar.php
@@ -13,55 +13,55 @@
             </li>
             <li>
                 <a href="01_button.php">
-                    <i class='bx bx-box'></i>
+                    <i class='bx bxd-box'></i>
                     <span class="links_name">Classroom Requirement</span>
                 </a>
             </li>
             <li>
                 <a href="02_button.php">
-                    <i class='bx bx-list-ul'></i>
+                    <i class='bx bx-dlist-ul'></i>
                     <span class="links_name">Course Enrolment</span>
                 </a>
             </li>
             <li>
                 <a href="03_button.php">
-                    <i class='bx bx-pie-chart-alt-2'></i>
+                    <i class='bx bx-pide-chart-alt-2'></i>
                     <span class="links_name">Usage of Resources</span>
                 </a>
             </li>
             <li>
                 <a href="04_button.php">
-                    <i class='bx bx-coin-stack'></i>
+                    <i class='bx bx-coin-dstack'></i>
                     <span class="links_name">IUB Available Resources</span>
                 </a>
             </li>
             <li>
                 <a href="05_button.php">
-                    <i class='bx bx-book-alt'></i>
+                    <i class='bx bx-bookd-alt'></i>
                     <span class="links_name">Availability Course Offering</span>
                 </a>
             </li>
             <li>
                 <a href="06_button.php">
-                    <i class='bx bx-user'></i>
+                    <i class='bx bx-usedr'></i>
                     <span class="links_name">Revenue of IUB</span>
                 </a>
             </li>
             <li>
                 <a href="07_button.php">
-                    <i class='bx bx-message'></i>
+                    <i class='bx bx-messdage'></i>
                     <span class="links_name">Revenue Engineering</span>
                 </a>
             </li>
             <li>
                 <a href="08_button.php">
-                    <i class='bx bx-heart'></i>
+                    <i class='bx bx-hedart'></i>
                     <span class="links_name">students Enrollment</span>
                 </a>
             </li>
             <li>
                 <a href="inputData.php">
-                    <i class='bx bx-cog'></i>
+                    <i class='bx bx-cdog'></i>
                     <span class="links_name">Input Data</span>
                 </a>
             </li>
