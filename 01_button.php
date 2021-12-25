33e6d7fc64e7b1db961963069717e72fd237c2e0
Nakibul islam <2022142@iub.edu.bd>
2021-12-25 08:08:48 +0600

    as

--- 01_button.php
+++ 01_button.php
@@ -138,43 +138,6 @@
 
             </table>
         </div>
-
-        <!-- Chart Data -->
-        <div class="chart">
-            <div class="chart1">
-                <canvas id="myChart"></canvas>
-            </div>
-            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-            <script>
-                const labels = [
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                ];
-
-                const data = {
-                    labels: labels,
-                    datasets: [{
-                        label: 'My First dataset',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: [0, 10, 5, 2, 20, 30, 45],
-                    }]
-                };
-
-                const config = {
-                    type: 'line',
-                    data: data,
-                    options: {}
-                };
-            </script>
-
-
-        </div>
     </section>
 
     <!-- JavaScript add -->
