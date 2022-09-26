237d0681ba3a39b4e26b82c53d73757b7d470539
Nakib00 <2022142@iub.edu.bd>
2022-09-26 20:40:11 +0600

    gg

--- 01_button.php
+++ 01_button.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <!-- Boxicons CDN Link -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Include CSS  -->
     <?php include('CSS/style.php') ?>
