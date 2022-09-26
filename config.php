7d6aefd36af6c800a95f8647b1cd031815874534
Nakib00 <2022142@iub.edu.bd>
2022-09-26 17:41:58 +0600

    ff

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+define('DB_NAME', 'webapp');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
