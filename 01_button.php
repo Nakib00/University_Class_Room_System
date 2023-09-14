90dfea44f9a93a85397b722075738c5553b9efb2
Nakib00 <2022142@iub.edu.bd>
2023-09-14 16:51:19 +0600

    update button 1

--- 01_button.php
+++ 01_button.php
@@ -16,6 +16,8 @@
     <?php include('CSS/Table_css.php') ?>
     <!-- Dropdown CSS  -->
     <?php include('CSS/dropdown.php') ?>
+    <!-- Add bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
     <title>TCMS</title>
 </head>
@@ -162,9 +164,14 @@
     </section>
 
     <!-- PieChart SUMMER 2009 CLASS SIZE 6-->
-    <section class="Graph">
-        <div id="piechart" style="width: 900px; height: 500px;"></div>
+    <section class="Graph pt-6 mb-5">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <div id="piechart" style="width: 900px; height: 500px;"></div>
+            </div>
+        </div>
     </section>
+
 
     <!-- JavaScript add -->
     <?php include('javascript/javascript.php') ?>
